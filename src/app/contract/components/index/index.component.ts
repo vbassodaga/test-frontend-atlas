092cdb3ef79cfa8efb6c60661e48b2734ce5fcb9
Vinícius Basso <vbasso.contato@gmail.com>
2022-06-01 20:07:45 -0300

    Modal creation, bugs and href correction

--- src/app/contract/components/index/index.component.ts
+++ src/app/contract/components/index/index.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  public showModal: boolean = false;
 
   constructor() { }
 
